dc7aa461c4aab19363c5ea198ac045de90f5505e
paumaneja <paumaneja@gmail.com>
2024-11-21 11:31:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 4.1-Spring_Framework_Basics.Introduccion_Spring
+Introduction to Spring Framework. HTTP, Postman, managing dependencies with Maven and Gradle.
---
+++ S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package cat.itacademy.s04.t01.n01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public String sayHello(@RequestParam(defaultValue = "UNKNOWN") String name) {
+        return "Hi, " + name + ". You are running a Maven project.";
+    }
+
+    @GetMapping({"/HelloWorld2", "/HelloWorld2/{name}"})
+    public String sayHello2(@PathVariable(required = false) String name) {
+        if (name == null) {
+            name = "UNKNOWN";
+        }
+        return "Hi, " + name + ". You are running a Maven project.";
+    }
+}
